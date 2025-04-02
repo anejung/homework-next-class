a93be52b8b0fd25c44b1ded2492b2a71f534b3e4
Your New Username <your.new.email@example.com>
2025-04-02 14:21:49 +0700

    try2

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,15 @@ import MenuBar from './component/MenuBar';
 
 
 export default function Home() {
+  return null
   return (
+
     <>
       <MenuBar />
       <div style={{ textAlign: 'center', padding: '2rem' }}>
       
         <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>
-          Hi, This is Ami's first website
+          {/* Hi, This is Ami's first website */}
         </h1>
         <p style={{ fontSize: '1.2rem' }}>
           Heading to the brighter world
